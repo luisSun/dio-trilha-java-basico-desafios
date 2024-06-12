b58029df0f6485ec1514381c1f56b6eae879d884
luisSun <luis.sun@gmail.com>
2024-06-12 18:39:31 -0300

    Operadores - Ternarios

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,9 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String nomeCompleto = "Luis" + "Fernando";
 
-        System.out.println(nomeCompleto);
+
+        
     }
     
 }
