b9ca7c75f6676048ecddf569dcc21bff4cbdf020
luisSun <luis.sun@gmail.com>
2024-06-12 19:06:22 -0300

    Operadores - Logicos

---
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -0,0 +1,23 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        boolean condicaoUm, condicaodois;
+
+        condicaoUm = true;
+        condicaodois = false;
+
+        if(condicaoUm && condicaodois){
+            System.out.println("As duas concições são verdadeiras");
+        }
+
+        if(condicaoUm || condicaodois){
+            System.out.println("Uma das concições são verdadeiras");
+        }
+
+
+        System.out.println("fim");
+
+    }
+}
+
---
+++ tipos-variaveis/src/OperadoresRelacional.java
@@ -0,0 +1,13 @@
+public class OperadoresRelacional {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "Verdadeiro" : "falso";
+        System.out.println(resultado); // This will print "falso"
+    }
+}
+
