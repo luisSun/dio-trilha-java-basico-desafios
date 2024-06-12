231f06e64c4a41e2e233a902add97da6c61b85ee
luisSun <luis.sun@gmail.com>
2024-06-12 17:05:31 -0300

    Aula de Tipos e Variáveis - 3

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salario = 2500.36;
+
+        short numeroCurto = 2;
+        int numeroNomal = numeroCurto;
+        short numeroCurto2 = (short) numeroNomal;
     }
 }
