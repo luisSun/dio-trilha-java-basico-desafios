9769c38326ab554e317087fb29243fe885e8716c
luisSun <luis.sun@gmail.com>
2024-06-14 18:40:24 -0300

    Argumentos - Scanner

--- JAVA-TERMINAL/src/AboutMe.java
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -1,14 +1,28 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     
     public static void main(String[] args) {
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
 
-        System.out.println("Ola meu nome é " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha Altura é " + altura);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu Sobrenome:");
+        String sobreNome = scanner.next();
+
+        System.out.println("Digite a sua Idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua Altura:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola meu nome seria " + nome + " " + sobreNome );
+        System.out.println("tenho " + idade + " anos");
+        System.out.println("tenho " + altura + " de altura");
 
     }
 }
